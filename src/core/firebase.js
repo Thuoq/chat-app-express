772907c58bf99263c39c8a4386adc8da9a4e3f30
Thuoq <thuonghamno3xz@gmail.com>
2023-07-05 21:52:11 +0700

    :+1: Remove fire base

--- src/core/firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin')
-
-const serviceAccount = require('../../firebase-service-account.json')
-
-const firebase = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://project-eco-db.firebaseio.com',
-})
-module.exports = firebase
